0ad99c7a014b7b2c87592f28684a7c84770310d7
wychwitch <wych@wychwit.ch>
2025-11-05 01:38:11 +0000

    favicon change

--- quartz.config.ts
+++ quartz.config.ts
@@ -16,7 +16,7 @@ const config: QuartzConfig = {
       provider: "plausible",
     },
     locale: "en-US",
-    baseUrl: "cry.wychwit.ch",
+    baseUrl: "golden.wychwit.ch",
     ignorePatterns: ["private", "templates", ".obsidian"],
     defaultDateType: "modified",
     theme: {
